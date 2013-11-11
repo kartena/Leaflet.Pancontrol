033e3ffcfe7fe43abf8bdb609109ea0f7d860e4d
Per Liedman <per@liedman.net>
2013-11-11 20:10:35 +0100

    Jshint settings and fixes

--- src/L.Control.Pan.js
+++ src/L.Control.Pan.js
@@ -9,36 +9,34 @@ L.Control.Pan = L.Control.extend({
 			container = L.DomUtil.create('div', className),
 			off = this.options.panOffset;
 
-		this._panButton('Up'   , className + '-up'   
-						, container, map, new L.Point(    0 , -off));
-		this._panButton('Left' , className + '-left' 
-						, container, map, new L.Point( -off ,  0));
-		this._panButton('Right', className + '-right'
-						, container, map, new L.Point(  off ,  0));
-		this._panButton('Down' , className + '-down'
-						, container, map, new L.Point(    0 ,  off));
+		this._panButton('Up'   , className + '-up',
+						container, map, new L.Point(    0 , -off));
+		this._panButton('Left' , className + '-left',
+						container, map, new L.Point( -off ,  0));
+		this._panButton('Right', className + '-right',
+						container, map, new L.Point(  off ,  0));
+		this._panButton('Down' , className + '-down',
+						container, map, new L.Point(    0 ,  off));
 
-    // Add pan control class to the control container
-    var controlContainer = L.DomUtil.get(map._controlCorners.topleft);
-    if(!L.DomUtil.hasClass(controlContainer, 'has-leaflet-pan-control'))
-    {
-      L.DomUtil.addClass(controlContainer, 'has-leaflet-pan-control');
-    }
-		
+		// Add pan control class to the control container
+		var controlContainer = L.DomUtil.get(map._controlCorners.topleft);
+		if(!L.DomUtil.hasClass(controlContainer, 'has-leaflet-pan-control')) {
+			L.DomUtil.addClass(controlContainer, 'has-leaflet-pan-control');
+		}
+
 		return container;
 	},
 
-  onRemove: function (map) {
-    // Remove pan control class to the control container
-    var controlContainer = L.DomUtil.get(map._controlCorners.topleft);
-    if(L.DomUtil.hasClass(controlContainer, 'has-leaflet-pan-control'))
-    {
-      L.DomUtil.removeClass(controlContainer, 'has-leaflet-pan-control');
-    }
-  },
+	onRemove: function (map) {
+		// Remove pan control class to the control container
+		var controlContainer = L.DomUtil.get(map._controlCorners.topleft);
+		if(L.DomUtil.hasClass(controlContainer, 'has-leaflet-pan-control')) {
+			L.DomUtil.removeClass(controlContainer, 'has-leaflet-pan-control');
+		}
+	},
 
-	_panButton: function (title, className, container, map, offset, text) {
-		var wrapper = L.DomUtil.create('div', className + "-wrap", container);
+	_panButton: function (title, className, container, map, offset) {
+		var wrapper = L.DomUtil.create('div', className + '-wrap', container);
 		var link = L.DomUtil.create('a', className, wrapper);
 		link.href = '#';
 		link.title = title;
@@ -46,23 +44,25 @@ L.Control.Pan = L.Control.extend({
 			.on(link, 'click', L.DomEvent.stopPropagation)
 			.on(link, 'click', L.DomEvent.preventDefault)
 			.on(link, 'click', function(){ map.panBy(offset); }, map)
-			.on(link, 'dblclick', L.DomEvent.stopPropagation)
+			.on(link, 'dblclick', L.DomEvent.stopPropagation);
 
 		return link;
 	}
 });
 
 L.Map.mergeOptions({
-    panControl: false
+	panControl: false
 });
 
 L.Map.addInitHook(function () {
-    if (this.options.panControl) {
+	if (this.options.panControl) {
 		this.panControl = new L.Control.Pan();
 		this.addControl(this.panControl);
 	}
 });
 
 L.control.pan = function (options) {
-    return new L.Control.Pan(options);
+	return new L.Control.Pan(options);
 };
+
+return L.Control.Pan;
